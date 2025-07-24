65197f2bdbbc3e60ebe89186a86fdc3431e0eb4c
ananthst7 <ananthst7@gmail.com>
2025-07-24 23:01:23 +0530

    added obj linked list pointer and takeString fn

--- object.c
+++ object.c
@@ -12,15 +12,18 @@
 static Obj* allocateObject(size_t size, ObjType type){
     Obj* object = reallocate(NULL, 0, size);
     object->type = type;
+
+    vm.objects = object;
+
     return object;
-};
+}
 
 static ObjString* allocateString(char* chars, int length){
     ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string->chars = chars;
     string->length = length;
     return string;
-};
+}
 
 ObjString* copyString(const char* chars, int length){
     char* heapChars = ALLOCATE(char ,length + 1);
@@ -29,4 +32,20 @@ ObjString* copyString(const char* chars, int length){
     
     return allocateString(heapChars,length);
 
-};
+}
+
+ObjString* takeString(char* chars, int length){
+    return allocateString(chars, length);
+}
+
+void printObject(Value value){
+
+    switch(OBJ_TYPE(value)){
+
+        case OBJ_STRING : 
+            printf("%s",AS_CSTRING(value));
+            break;
+    }
+
+
+}
