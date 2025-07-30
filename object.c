3aa650dcf8149edd9b9e73791b386c0a3cdce751
ananthst7 <ananthst7@gmail.com>
2025-07-30 16:11:50 +0530

    seeing if a string is interned before allocating

--- object.c
+++ object.c
@@ -3,6 +3,7 @@
 
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 #include "vm.h"
 
@@ -23,6 +24,8 @@ static ObjString* allocateString(char* chars, int length, uint32_t hash){
     string->chars = chars;
     string->length = length;
     string->hash = hash;
+    tableSet(&vm.strings, string, NIL_VAL);
+
     return string;
 }
 
@@ -38,6 +41,10 @@ static uint32_t hashString(const char* key, int length){
 
 ObjString* copyString(const char* chars, int length){
     uint32_t hash = hashString(chars, length);
+    ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+
+    if(interned != NULL) return interned;
+
     char* heapChars = ALLOCATE(char ,length + 1);
     memcpy(heapChars, chars, length);
     heapChars[length] = '\0';
@@ -48,6 +55,13 @@ ObjString* copyString(const char* chars, int length){
 
 ObjString* takeString(char* chars, int length){
     uint32_t hash = hashString(chars,length);
+
+    ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+    if(interned != NULL){
+        FREE_ARRAY(char, chars, length + 1);
+        return interned;
+    }
+
     return allocateString(chars, length, hash);
 }
 
