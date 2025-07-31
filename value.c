318839afc5f2981c9bf8bfb00ae791160feb81fa
ananthst7 <ananthst7@gmail.com>
2025-07-31 20:17:26 +0530

    fixed printValue statement, now runs numbers

--- value.c
+++ value.c
@@ -39,7 +39,7 @@ void printValue(Value value)
 
         case VAL_NIL : printf("nil"); break;
         case VAL_BOOL : printf(AS_BOOL(value) ? "true" : "false"); break;
-        case VAL_NUMBER : printf("%g", AS_NUMBER(value));
+        case VAL_NUMBER : printf("%g", AS_NUMBER(value)); break;
         case VAL_OBJ : printObject(value); break;
     
     }
