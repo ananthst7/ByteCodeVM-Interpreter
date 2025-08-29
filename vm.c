a24a7fe44525bfb4f34afa31930f7949d4aa7b03
ananthst7 <ananthst7@gmail.com>
2025-08-29 19:56:29 +0530

    added local variables and if statement

--- vm.c
+++ vm.c
@@ -87,6 +87,8 @@ static InterpretResult run()
 {
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk->constants.value[READ_BYTE()])
+    #define READ_SHORT() \
+        (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
     #define READ_STRING()  AS_STRING(READ_CONSTANT())
     #define BINARY_OP(valueType, op) \
         do{ \
@@ -215,6 +217,18 @@ static InterpretResult run()
             case OP_PRINT:{
                 printValue(pop());
                 printf("\n");
+                break;
+            }
+
+            case OP_JUMP:{
+                uint16_t offset = READ_SHORT();
+                vm.ip += offset;
+                break;
+            }
+
+            case OP_JUMP_IF_FALSE:{
+                uint16_t offset = READ_SHORT();
+                if(isFalsey(peek(0))) vm.ip += offset;
                 break;
             }
 
